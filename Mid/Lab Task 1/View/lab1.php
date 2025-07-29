e16e6b832f877da36f3e58110d30192e11b78c76
naimulislamsaikat <nzsaikat06@gmail.com>
2025-07-29 23:52:24 +0600

    Basic Form

--- Mid/Lab Task 1/View/lab1.php
+++ Mid/Lab Task 1/View/lab1.php
@@ -1,1 +1,65 @@
+<!DOCTYPE html>
+<html>
+    <title> Registration from </title>
+    <body>
+        <center>
+            <h1 style="color:blue;"> AIUB </h1>
+            <h2 style="color:blue;"> Registration Form </h2>
+        </center>
+        
+        <h3 align="left"> Complete the Registration </h3>
 
+        <table>
+            
+            <tr> <td>
+                Full name: </td> <td> <input type="text"> </td>
+            <tr> <td>
+                Email: </td> <td> <input type="text"> </td>
+            <tr> <td>
+                Password: </td> <td> <input type="text"> </td>
+
+            <tr>
+        
+            <td> Gender: </td>
+                <td> <input type="radio" name="des">Male
+                    <input type="radio"name="des">Other <br> </td>
+            </tr>
+
+            <tr>
+                <td> Lenguage known: </td>
+                <td> <input type="checkbox"> English
+                    <input type="checkbox"> Bangla
+                    <input type="checkbox"> Hindi <br> </td>
+            </tr>
+        
+            <tr>
+                <td> Country: </td>
+                <td><Select>
+                    <Option value="=Select-">-Select-</Option>
+                    <Option value="Bangladesh">Bangladesh</Option>
+                    <Option value="India">India</Option> </td>
+            </tr>
+        
+            <tr>
+                <td> Date of Birth: </td>
+                <td> <input type="date">
+            </tr>
+
+            <tr> 
+                <td> Upload photos: </td>
+                <td> <input type="file"> </td>
+            </tr>
+
+            <tr>
+                <td> Comments: </td>
+                <td> <textarea name="" id="" cols="40" rows="5"></textarea></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td><input type="submit" value=Register></td>
+            </tr>
+
+        </table>
+    </body>
+</html>
